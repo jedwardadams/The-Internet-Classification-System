528e7da283deeaed8d09d34975ee9acac2fbedf9
J. M. Rigden <jasonrigden@gmail.com>
2014-02-06 21:31:43 -0800

    Create directory.js

---
+++ directory.js
@@ -0,0 +1,19 @@
+var ICS-Directory = {
+    top_level: ["art", "business", "food", "games", "religion", "sports", "technology"],
+
+    "art": ["viual arts", "literature"],
+    "business": ["careers", "investing"],
+    "games": ["video games", "tabletop games"],
+    "religion": ["buddhism", "christianity", "hinduism", "islam", "judaism", "athiesm"],
+    "technology": ["gadgets", "tech news"],
+
+    "sports": ["soccer", "football", "hockey", "baseball"],
+    "football": ["NFL", "NCAA"],
+    "NFL": ["seattle seahawks", "san francisco 49ers", "oakland raiders", "greenbay packers"],
+
+    "geo_location": ["north america", "africa", "asia", "europe"],
+    "north america": ["usa", "canada", "mexico"],
+    "usa": ["washington", "oregon", "idaho", "california"],
+    "washington": ["seattle", "spokane", "tacoma"],
+    "seattle": ["seattle mariners", "seattle seahawks", "seattle sounders"],
+}
